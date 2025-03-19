22246ecdbb8d318ffebb6859011851a38f33f584
omgimanerd <alvin@omgimanerd.tech>
2025-03-18 22:29:27 -0400

    Update betterNoteBlock.js with client script that cancels ghost block placement.

--- better-note-block/server_scripts/betterNoteBlock.js
+++ better-note-block/server_scripts/betterNoteBlock.js
@@ -1,27 +1,24 @@
 // priority: 100
 
 BlockEvents.rightClicked('minecraft:note_block', (e) => {
-  const { player, item, block, level } = e
-  // Only works if shift clicking with an empty hand. Otherwise, if the player
-  // is holding a block it will place the block for an instant.
-  if (!player.isCrouching() || item.id !== 'minecraft:air') return
+  const { player, block, level } = e
+  if (!player.isCrouching()) return
 
   // Set the block note to the previous note
   // https://minecraft.fandom.com/wiki/Note_Block
   const TOTAL_NOTES = 25
-  const properties = block.properties
+  const bp = block.properties
   const newNote =
-    (parseInt(properties.getOrDefault('note', 0), 10) + 24) % TOTAL_NOTES
-  const instrument = properties.getOrDefault('instrument', 'harp')
-  block.set(block.id, {
+    (parseInt(bp.getOrDefault('note', 0), 10) + (TOTAL_NOTES - 1)) % TOTAL_NOTES
+  const instrument = bp.getOrDefault('instrument', 'harp')
+  block.set(block.getId(), {
     instrument: new String(instrument),
     note: new String(newNote),
-    powered: new String(properties.getOrDefault('powered', false)),
+    powered: new String(bp.getOrDefault('powered', false)),
   })
 
   // Display the note particle like the normal right click event
   const soundEvent = `block.note_block.${instrument}`
-  // Pitch formula, which is the same as the /playsound command
   const pitch = Math.pow(2, (newNote - 12) / 12)
   level.playSound(
     null, // player
